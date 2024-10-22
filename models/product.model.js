24e074e844442694ac293ba31d6dfa0306e95131
mariialbano <marianatalbano@gmail.com>
2024-10-22 20:49:30 -0300

    Inserção de outras variáveis e finalização do Front-End

--- models/product.model.js
+++ models/product.model.js
@@ -7,6 +7,28 @@ const ProductSchema = mongoose.Schema(
             required: [true, "Name of the client"],
         },
 
+        client_email: {
+            type: String,
+            required: [true, "E-mail"],
+        },
+
+        client_phone: {
+            type: Number,
+            required: true,
+            default: 0
+        },
+
+        client_cpf: {
+            type: Number,
+            required: true,
+            default: 0
+        },
+
+        client_adress: {
+            type: String,
+            required: [true, "Name of the adress"],
+        },
+
         name: {
             type: String,
             required: [true, "Name of the product"],
@@ -22,6 +44,16 @@ const ProductSchema = mongoose.Schema(
             type: Number,
             required: true,
             default: 0
+        },
+
+        payment_method: {
+            type: String,
+            required: [true, "Payment Method"],
+        },
+
+        status: {
+            type: String,
+            required: [true, "Status"],
         },
 
         image: {
